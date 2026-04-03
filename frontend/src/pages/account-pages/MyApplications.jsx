1f37627538deaf3745a7bb8f289875e196bb959c
DominikMat <dominikmat@student.agh.edu.pl>
2026-04-03 19:29:33 +0200

    harmonogram na stronie do wnioskow

--- frontend/src/pages/account-pages/MyApplications.jsx
+++ frontend/src/pages/account-pages/MyApplications.jsx
@@ -7,6 +7,7 @@ import '../../styles/MyApplications.css'
 export default function MyApplications({}) {
     const [activeCardId, setActiveCardId] = React.useState(null);
 
+    /* Karta na wyswietlenie jednego wniosku */
     const ApplicationCard = ({ 
         id,
         documentName='dokument wysłany przez użytkownika', 
@@ -49,6 +50,7 @@ export default function MyApplications({}) {
                 </div>
                 <p> Zarządzaj swoimi procesami rekrutacyjnymi i monitoruj statusy dokumentów w jednym miejscu. </p>
 
+                {/* Dokumenty niewyslane */}
                 <div className='bg-panel application-card'>
                     <div className='section-title application-section-title'>
                         Niewysłane
@@ -56,6 +58,7 @@ export default function MyApplications({}) {
                     <ApplicationCard id="card-1" unfinished={true} statusText='Oczekuje wypełnienia' statusId='unfinished'/>
                 </div>
                 
+                {/* Wnioski aktywne */}
                 <div className='bg-panel application-card'>
                     <div className='section-title application-section-title'>
                         Aktywne wnioski
@@ -66,7 +69,66 @@ export default function MyApplications({}) {
             </div>
 
             <div className='account-column' id='account-page-column-right'>
-                ...
+                {/* Panel Harmonogramu */}
+                <div className="right-panel harmonogram-panel">
+                    <h3 className="panel-title text-gold">HARMONOGRAM REKRUTACJI</h3>
+                    
+                    <div className="timeline-container">
+                        {/* To jest ta główna linia, która ciągnie się do samego dołu */}
+                        <div className="timeline-line"></div>
+
+                        <div className="timeline-item completed">
+                            <div className="timeline-icon bg-teal">
+                                <span className="material-symbols-outlined">check</span>
+                            </div>
+                            <div className="timeline-content">
+                                <h4>SKŁADANIE WNIOSKÓW</h4>
+                                <p>Zakończono: 10 Października</p>
+                            </div>
+                        </div>
+
+                        <div className="timeline-item in-progress">
+                            <div className="timeline-icon bg-yellow">
+                                <span className="material-symbols-outlined">sync</span>
+                            </div>
+                            <div className="timeline-content">
+                                <h4>WERYFIKACJA DOKUMENTÓW</h4>
+                                <p className="text-yellow">W toku: Przewidywany koniec 20 Paź</p>
+                            </div>
+                        </div>
+
+                        <div className="timeline-item upcoming">
+                            <div className="timeline-icon bg-grey"></div>
+                            <div className="timeline-content">
+                                <h4>OGŁOSZENIE WYNIKÓW</h4>
+                                <p>25 Października</p>
+                            </div>
+                        </div>
+
+                        <div className="timeline-item upcoming">
+                            <div className="timeline-icon bg-grey"></div>
+                            <div className="timeline-content">
+                                <h4>WPIS NA STUDIA</h4>
+                                <p>Listopad 2023</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Panel Pomocy */}
+                <div className="right-panel help-panel">
+                    <h3 className="help-title">Potrzebujesz pomocy?</h3>
+                    
+                    <a href="#faq" className="help-link">
+                        <span>FAQ</span>
+                        <span className="material-symbols-outlined icon-small">open_in_new</span>
+                    </a>
+                    
+                    <a href="#message" className="help-link">
+                        <span>WYŚLIJ WIADOMOŚĆ DO KOORDYNATORA</span>
+                        <span className="material-symbols-outlined icon-small">mail</span>
+                    </a>
+                </div>
             </div>
         </div>
     );
