5a29875aeba8b94756e79e69c52d7c335db49733
Grzegorz Grudzień <ggrudzien@student.agh.edu.pl>
2026-05-17 14:00:42 +0200

    login timeout changed to 30 min

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -152,7 +152,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "AUTH_HEADER_TYPES": ("Bearer",),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
